8de09ad21c2e035518301f1e9f57d71a5f0fc71f
Zeeshan Khan <cloudzee2024@gmail.com>
2025-09-24 21:01:12 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,24 @@
-terraform {
-  required_version = ">= 1.6.0"
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = ">= 3.110.0"
+variable "subscription_id" { type = string }
+variable "tenant_id" { type = string }
+variable "location" { type = string default = "eastus" }
+variable "prefix" { type = string default = "cz3tier" }
+
+
+variable "address_spaces" {
+    description = "CIDRs for hub/spoke"
+    type = object({
+        hub = string
+        spoke = string
+    })
+    default = {
+        hub = "10.10.0.0/16"
+        spoke = "10.20.0.0/16"
     }
-    random = {
-      source = "hashicorp/random"
-      version = ">= 3.6.0"
-    }
-  }
+}
+
+
+variable "dns_host" {
+    description = "Hostname used on Ingress (configure your DNS A record to Firewall public IP)"
+    type = string
+    default = "app.cloudzee.local"
 }
