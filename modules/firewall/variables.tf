806798dd19f4605ceb9d0105c6f48dd8c0d7a236
Zeeshan Khan <cloudzee2024@gmail.com>
2025-09-24 21:13:31 +0530

    Create variables.tf

---
+++ modules/firewall/variables.tf
@@ -0,0 +1,5 @@
+variable "rg_name" { type = string }
+variable "location" { type = string }
+variable "prefix" { type = string }
+variable "hub_vnet_id" { type = string }
+variable "appgw_priv_ip" { type = string }
