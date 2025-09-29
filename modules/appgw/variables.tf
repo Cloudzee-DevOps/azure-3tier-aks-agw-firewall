b03398ef6ab158f37d5446880f6e32975dfa0297
Zeeshan Khan <cloudzee2024@gmail.com>
2025-09-29 20:14:52 +0530

    Create variables.tf

---
+++ modules/appgw/variables.tf
@@ -0,0 +1,5 @@
+variable "rg_name" { type = string }
+variable "location" { type = string }
+variable "prefix" { type = string }
+variable "spoke_vnet_id" { type = string }
+variable "appgw_subnet_id"{ type = string }
