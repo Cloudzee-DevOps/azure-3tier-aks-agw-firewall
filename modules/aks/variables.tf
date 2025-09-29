b41f80dbbdcc76e0d8f7164c295ee7e61b143c4b
Zeeshan Khan <cloudzee2024@gmail.com>
2025-09-29 20:18:53 +0530

    Create variables.tf

---
+++ modules/aks/variables.tf
@@ -0,0 +1,5 @@
+variable "rg_name" { type = string }
+variable "location" { type = string }
+variable "prefix" { type = string }
+variable "aks_subnet_id" { type = string }
+variable "appgw_id" { type = string }
